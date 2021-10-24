687d79352c3cf39f7147b078b14811e80e5e2c1b
vishnu ekkar <39240846+viekkar@users.noreply.github.com>
2021-10-24 11:23:22 +0530

    Update FYERAPI1.py
    removed stock code

--- FYERAPI1.py
+++ FYERAPI1.py
@@ -23,34 +23,5 @@ response = app_session.auth()
 
 
 print(response)
-# json_object = json.dumps(response)  
-# print(json_object) 
-
-# stocks = json_object["authorization_code"]
-# print(stocks)
-
-# y = json_object
-
-# print(y)
-# # print(type(y))
-
-# last = y.strip('{"code": 200, "data": {"authorization_code": ')
-
-# first = last.strip('"}, "message": ""}')
-
-# final = "e"+first
-# # print(final)
-
-
-# add = "https://api.fyers.in/api/v1/genrateToken?authorization_code="+final+"&appId=5SNVZTE1PX"
-
-
-# response = requests.get(add)
-
-# # print response
-# # print(response)
-
-# # print url
-# print(response.text)
 
 
